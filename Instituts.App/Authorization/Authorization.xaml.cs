feb841efe0b0e81315deab2450ffd7bebfea2a68
Pavel Popov <legeon48@mail.ru>
2022-02-17 12:13:12 +0300

    Add authorization

---
+++ Instituts.App/Authorization/Authorization.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows;
+
+namespace Instituts.App.Authorization
+{
+    public partial class Authorization : Window
+    {
+        public Authorization()
+        {
+            InitializeComponent();
+
+            
+        }
+    }
+}
