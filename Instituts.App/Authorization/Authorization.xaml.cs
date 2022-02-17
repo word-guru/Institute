86b16db26c833724ba86e71eae11c338d87e0849
Pavel Popov <legeon48@mail.ru>
2022-02-17 13:31:24 +0300

    Update authorization

--- Instituts.App/Authorization/Authorization.xaml.cs
+++ Instituts.App/Authorization/Authorization.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Instituts.DB.Lib;
+using System;
 using System.Windows;
 
 namespace Instituts.App.Authorization
@@ -10,6 +11,32 @@ namespace Instituts.App.Authorization
             InitializeComponent();
 
             
+        }
+
+        private void ButtonAuth_Click(object sender, RoutedEventArgs e)
+        {
+            /*var login = InputLogin.Text;
+            var password = InputPassword.Password;
+
+            var db = new DbInstituts();
+            var account = db.TabAccounts.FirstOrDefault(a => a.Login == login && a.Password == password);
+
+            if (account is null)
+            {
+                MessageBox.Show("Неправильно ввели логин и пароль!", "!!! ERROR !!!");
+            }
+            else
+            {
+                MessageBox.Show("Добро пожаловать!");
+                var main = new MainWindow(account);
+                main.Show();
+                Close();
+            }*/
+        }
+
+        private void ButtonCancel_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
